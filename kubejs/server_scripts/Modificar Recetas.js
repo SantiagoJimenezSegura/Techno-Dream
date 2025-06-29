0d5d0362a05a19da8df3be31a83a64ef736cd57e
Haruk0 <ren.gomezn@gmail.com>
2025-06-29 16:07:15 -0600

    Delete Modificar Recetas.js

--- kubejs/server_scripts/Modificar Recetas.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes(event => {
-    event.replaceInput({ type: 'gtceu:primitive_blast_furnace' }, '#forge:dust/coal', 'gtceu:coal_dust') 
-})
-
-ServerEvents.recipes(event => {
-    event.replaceInput({ type: 'gtceu:primitive_blast_furnace' }, '#forge:storage_block/charcoal', 'gtceu:charcoal_block') 
-})
